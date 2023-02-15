8556272017e55ba08521601bbe43857c53fe7216
“Aleksandr” <smylnikov@mail.ru>
2023-02-15 20:24:23 +0300

    решил task2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿ double a = 7, b = 78, c = 22;
+            
+                if (a > b && a > c)
+                    Console.WriteLine("Nubmer Max = " + a); 
+                else if (b > c)
+                    Console.WriteLine("Number Max = " + b); 
+                else
+                    Console.WriteLine("Number Max = " + c); 
+            
+            Console.ReadLine();
